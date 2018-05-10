5af5969b24fed3f156fbb3bf5651d7b62c372a5d
iiita2011021 <mayurkumarbajaj@gmail.com>
2018-05-11 01:11:18 +0530

    WA: not getting accepted

--- codechef/RD19.java
+++ codechef/RD19.java
@@ -2,49 +2,94 @@ import java.io.*;
 import java.util.*;
 
 class RD19{
+    
+    public int max(int[] arr, int n){
+        // returns the max value from the array
+        int x = arr[0];
+        for(int i=1; i<n; i++){
+            if(x < arr[i]){
+                x = arr[i];
+            }
+        }
+        return x;
+    }
+    public int min(int[] arr, int n){
+        // returns the max value from the array
+        int x = arr[0];
+        for(int i=1; i<n; i++){
+            if(x > arr[i]){
+                x = arr[i];
+            }
+        }
+        return x;
+    }
 
-    // public int gcd(int n1, int n2){
-    //     for(int i = 1; i <= n1 && i <= n2; ++i)
-    //     {
-    //         // Checks if i is factor of both integers
-    //         if(n1 % i==0 && n2 % i==0)
-    //             gcd = i;
-    //     }
-    //     return gcd;
-    // }
+    public Set<Integer> getFactors(int x){
+        Set<Integer> factors = new HashSet<Integer>();
+        for(int i=2; i<=x; i++){
+            while(x%i==0){
+                factors.add(i);
+                x = x/i;
+            }
+        }
+        return factors;
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        RD19 obj = new RD19();
         int t = sc.nextInt();
 
         for(int testCase=0; testCase<t; testCase++){
             int n = sc.nextInt();
-            int deletions = 0;
+            int max,min,deletions = 0;
             int[] seq = new int[n];
+            Set<Integer> factorsList;
+            
 
             for(int i=0; i<n; i++){
                 seq[i] = sc.nextInt();
             }
             // System.out.printf("Original arr[] : %s\n",Arrays.toString(seq));
-            Arrays.sort(seq);
-            // System.out.printf("Sorted arr[] : %s\n",Arrays.toString(seq));
-            for(int i=0; i<n-1 ; i++) {
-                for(int j=i+1; j<n ; j++){
-                    if(seq[i] == 1){
-                        continue;
-                    }
-                    if(seq[j]%seq[i] == 0){
-                        deletions +=1;
+            max = obj.max(seq, n);
+            min = obj.min(seq, n);
+            
+            boolean[] factorFlag = new boolean[max+1];
+            
+
+            for(int i=0; i<n; i++){
+                factorsList = obj.getFactors(seq[i]);
+                for (Integer factor : factorsList) {
+                    // System.out.format("%d\t",factor);
+                    if(factorFlag[factor]){
                         seq[i] = -1;
+                        deletions +=1;
                         break;
+                    }else{
+                        factorFlag[factor] = true;
                     }
                 }
+                // System.out.println("x");
             }
-            // System.out.printf("Modified arr[] : %s\n",Arrays.toString(seq));
+            
+            // for(int i=2; i<max ; i++) {
+            //     // i is a devisor from 2 to max
+            //     for(int j=0; j<n ; j++){
+            //         // j is elements of array
+            //         if(seq[j]%i == 0){
+            //             // need to delete this element because the gcd is i which is greater than 2
+            //             deletions +=1;
+            //             seq[j] = -1;
+            //         }
+            //     }
+            // }
+            System.out.printf("Modified arr[] : %s\n",Arrays.toString(seq));
+            // System.out.printf("factorFlag b: %s\n", Arrays.toString(factorFlag));
             if(deletions >= n){
                 deletions = -1;
             }
-            if((n - deletions) == 1 && seq[0] != 1){
+            if((n - deletions) == 1 && min != 1){
+                // System.out.print("here");
                 deletions = -1;
             }
             System.out.printf("%d\n",deletions);
