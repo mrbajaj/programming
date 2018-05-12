30ce257c33dedca8742b2cdfaf10b770369fe894
iiita2011021 <mayurkumarbajaj@gmail.com>
2018-05-12 14:21:00 +0530

    recursive solution for merging sorted linked list

--- algorithms/MergeSortedLinkedLists.java
+++ algorithms/MergeSortedLinkedLists.java
@@ -141,3 +141,45 @@ class MergeSortedLinkedLists{
     }
 
 }
+
+
+
+// other recursive solution
+/*
+Submitted on hackerank
+
+//   Merge two linked lists 
+//   head pointer input could be NULL as well for empty list
+//   Node is defined as 
+//   class Node {
+//      int data;
+//      Node next;
+//   }
+
+
+Node mergeLists(Node headA, Node headB) {
+    // This is a "method-only" submission. 
+    // You only need to complete this method 
+   if (headA == null && headB == null) 
+       return null;
+   else if (headA == null) 
+       return headB;
+   else if (headB == null) 
+       return headA;
+
+   if(headA.data <= headB.data)
+       headA.next = mergeLists(headA.next, headB);
+   else {
+       Node temp = headB;
+       headB = headB.next;
+       temp.next = headA;
+       headA = temp;
+       headA.next = mergeLists(headA.next, headB);
+   }
+
+   return headA;
+}
+
+
+
+*/
