bf973b32debdbd8bb813d10f82f59f214b198731
mrbajaj <mayurkumarbajaj@gmail.com>
2018-08-12 17:40:51 +0530

    Add: aug secod problem accepted

--- codechef/SHKNUM.java
+++ codechef/SHKNUM.java
@@ -1,133 +1,69 @@
 import java.util.*;
 
-
 class SHKNUM{
     static Map<Integer,Integer> powerMap = new HashMap<>();
+    static List<Integer> sumList = new ArrayList<>();
     
     public static void main(String[] args) {
         initializeHigherPowerOfTwo();
+        initializeSumList();
+        
         Scanner sc = new Scanner(System.in);
         int testCases = sc.nextInt();
-        int x=0,y=0, check;
-        int fPerNum, rPerNum, fDiff, rDiff;
         for(int t=0; t < testCases; t++){
-            int N = sc.nextInt();
-            
-            x =0;
-            y = getMaxPower(N);
-            
-            //forward check
-            check = N;
-            while(true){
-                if(isPerfectNumber(x,y,check)){
-                    break;
-                }
-                check++;
-            }
-            fPerNum = check;
-            
-            if(fPerNum != N){
-                //reverse check
-                check = N;
-                while(true && check != 0){
-                    if(isPerfectNumber(x,y,check)){
-                        break;
-                    }
-                    check--;
-                }
-                rPerNum = check;
-            }else{
-                rPerNum = 0;
-            }
-            
-            fDiff = fPerNum - N;
-            rDiff = N - rPerNum;
-            // System.out.println(fPerNum);
-            // System.out.println(rPerNum);
-            if(fDiff < rDiff){
-                System.out.println(fDiff);
-            }else{
-                System.out.println(rDiff);
-            }
+            int N = sc.nextInt(); //Integer.parseInt(sc.nextLine());//
+            Integer nearestNeighbourDist;
+            nearestNeighbourDist = Math.abs(getNearestNeighbourDist(N));
+            System.out.println(nearestNeighbourDist);
         }
-        // System.out.println(powerMap.toString());
         sc.close();
     }
-    public static boolean isPerfectNumber(int x, int y, int checkMe){
-        boolean isPerfectNumber = false;
-        int tmp;
-        while(x<y){
-            if(x > 29 || y > 29){
-                return false;
-            }
-            if(x==y){
-                x++;
-                continue;
-            }
-            
-            // tmp = (int)Math.pow(2, x) +(int) Math.pow(2, y);
-            tmp = getTowsPower(x) + getTowsPower(y);
-            if(tmp == checkMe){
-                isPerfectNumber = true;
+    public static Integer getNearestNeighbourDist(int N){
+        Integer sum1 = sumList.get(0), sum2= sumList.get(sumList.size()-1);
+        
+        for (int i=0; i< sumList.size()-1; i++) {
+            if(sumList.get(i) > N){
                 break;
-            }else if(tmp > checkMe){
-                y--;
-            }else{
-                x++;
+            }
+            sum1 = sumList.get(i);
+        }
+        for (int i=sumList.size()-1; i >= 0; i--) {
+            if(sumList.get(i) < N){
+                break;
+            }
+            sum2 = sumList.get(i);
+        }
+        // System.out.format("%d %d %d\n",sum1,sum2,N);
+        if( (N - sum2) < (sum1 - N)){
+            // System.out.print("Case1: ");
+            return N - sum1;
+        }else{
+            // System.out.print("Case2: ");
+            return sum2 - N;
+        }
+    }
+    
+    public static void initializeHigherPowerOfTwo(){
+        powerMap.put(30, 1073741824);
+        powerMap.put(29, 536870912); powerMap.put(28, 268435456); powerMap.put(27, 134217728); powerMap.put(26, 67108864);
+        powerMap.put(25, 33554432);  powerMap.put(24, 16777216);  powerMap.put(23, 8388608);   powerMap.put(22, 4194304);   
+        powerMap.put(21, 2097152);   powerMap.put(20, 1048576);   powerMap.put(19, 524288);    powerMap.put(18, 262144);   
+        powerMap.put(17, 131072);    powerMap.put(16, 65536);     powerMap.put(15, 32768);     powerMap.put(14, 16384);    
+        powerMap.put(13, 8192);      powerMap.put(12, 4096);      powerMap.put(11, 2048);      powerMap.put(10, 1024);   
+        powerMap.put(9, 512);        powerMap.put(8, 256);        powerMap.put(7, 128);        powerMap.put(6, 64);   
+        powerMap.put(5, 32);         powerMap.put(4, 16);         powerMap.put(3, 8);          powerMap.put(2, 4);
+        powerMap.put(1, 2);          powerMap.put(0, 1);
+    }
+
+    public static void initializeSumList(){
+        List<Integer> powerValues = new ArrayList<Integer>(powerMap.values());
+
+        for(int i=0; i < powerValues.size(); i++){
+            for(int j=i+1; j < powerValues.size(); j++){
+                sumList.add(powerValues.get(i) + powerValues.get(j));
             }
         }
-
-        return isPerfectNumber;
-    }
-    public static int getMaxPower(int N){
-        int pow = 1;
-        while(N != 1){
-            pow++;
-            N = N/2;
-        }
-        return pow;
-    }
-
-    public static int getTowsPower(int p){
-        if(powerMap.containsKey(p)){
-            return powerMap.get(p);
-        }
-        // int value = (int)Math.pow(2,p);
-        int value = 1 << p;
-        powerMap.put(p, value);
-        return value;
-    }
-    public static void initializeHigherPowerOfTwo(){
-        powerMap.put(29, 536870912);
-        powerMap.put(28, 268435456);
-        powerMap.put(27, 134217728);
-        powerMap.put(26, 67108864);
-        powerMap.put(25, 33554432);
-        powerMap.put(24, 16777216);   
-        powerMap.put(23, 8388608);   
-        powerMap.put(22, 4194304);   
-        powerMap.put(21, 2097152);   
-        powerMap.put(20, 1048576);   
-        powerMap.put(19, 524288);   
-        powerMap.put(18, 262144);   
-        powerMap.put(17, 131072);   
-        powerMap.put(16, 65536);   
-        powerMap.put(15, 32768);   
-        powerMap.put(14, 16384);   
-        powerMap.put(13, 8192);   
-        powerMap.put(12, 4096);   
-        powerMap.put(11, 2048);   
-        powerMap.put(10, 1024);   
-        powerMap.put(9, 512);   
-        powerMap.put(8, 256);
-        powerMap.put(7, 128);   
-        powerMap.put(6, 64);   
-        powerMap.put(5, 32);   
-        powerMap.put(4, 16);
-        powerMap.put(3, 8);
-        powerMap.put(2, 4);
-        powerMap.put(1, 2);
-        powerMap.put(0, 1);
-
+        Collections.sort(sumList);
+        // System.out.println(sumList);
     }
 }
