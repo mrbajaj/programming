20f643506bbe95f3c0835729c0f14941bd6a2afc
mayurkumarbajaj <mayurkumarbajaj@gmail.com>
2018-10-07 14:04:50 +0530

    Update: weinvest problem

--- Seats.java
+++ Seats.java
@@ -1,12 +1,14 @@
 import java.util.*;
+class SeatsArangement{
+    Integer count,maxrow,maxcol;
+    List<Integer> inputMAT = new ArrayList<Integer>();
+    SeatsArangement(){
+        this.count = 0; this.maxrow = 0; this.maxcol = 0;
+    }
 
-
-class Seats{
-    public static void main(String args[]){
+    public void getInput(){
         Scanner sc = new Scanner(System.in);
-
-        List<Integer> mat = new ArrayList<Integer>();
-        Integer val,count=0,maxrow=0,maxcol=0;
+        Integer val;
         while(true)
         {
             String line = new String(sc.nextLine());
@@ -17,32 +19,45 @@ class Seats{
                 String[] integerStrings = line.split(" "); 
                 for (int i = 0; i < integerStrings.length; i++){
                     val = Integer.parseInt(integerStrings[i]);
-                    mat.add(val);
+                    this.inputMAT.add(val);
                     count++;
                 }
             }
         }
-        System.out.println(mat.toString());
+        System.out.println(this.inputMAT.toString());
         if(count%2 != 0){
-            count=count-1; 
-            mat.remove(count.intValue());
+            this.count=this.count-1; 
+            this.inputMAT.remove(this.count.intValue());
         }
-        for(int i =0; i < count ; i++){
-            val = mat.get(i);
-            if(i%2 == 0 && maxrow < val){
-               maxrow = val; 
+        for(int i =0; i < this.count ; i++){
+            val = this.inputMAT.get(i);
+            if(i%2 == 0 && this.maxrow < val){
+                this.maxrow = val; 
             }
-            if(i%2 == 1 && maxcol < val){
-                maxcol = val;
+            if(i%2 == 1 && this.maxcol < val){
+                this.maxcol = val;
             }
         }
 
-
-        System.out.println(mat.toString());
-        System.out.println(maxrow);
-        System.out.println(maxcol);
+        System.out.println(this.inputMAT.toString());
+        System.out.println(this.maxrow);
+        System.out.println(this.maxcol);
         
         sc.close();
+    }
+
+    // public void getBluePrintofMAP(){
+    //     var actualCols = (this.count / 2 ) + this.count - 1;
+    //     var actualRows = this.maxrow;
+        
+
+    // }
+}
+
+public class Seats{
+    public static void main(String args[]){
+        SeatsArangement sa = new SeatsArangement();
+        sa.getInput();
     }
 
 }
