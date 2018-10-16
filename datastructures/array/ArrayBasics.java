a8b83d302f8f51f54ad9162b55d10b5c9a26c4b3
mrbajaj <mayurkumarbajaj@gmail.com>
2018-10-17 00:10:39 +0530

    Add: array basics java

---
+++ datastructures/array/ArrayBasics.java
@@ -0,0 +1,94 @@
+import java.io.*;
+import java.util.*;
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter N:");
+        int N = sc.nextInt();
+        System.out.print("Enter M:");
+        int M = sc.nextInt();
+        System.out.print("Enter P:");
+        int P = sc.nextInt();
+
+        HelperClass helper = new HelperClass();
+        System.out.print("-----------One D Array--------------\n");
+        helper.ArrayProcesser(N,sc);
+        
+        System.out.print("-----------Two D Array--------------\n");
+        helper.ArrayProcesser(N,M,sc);
+
+        System.out.print("-----------Three D Array--------------\n");
+        helper.ArrayProcesser(N,M,P,sc);
+        
+        
+        sc.close();
+    }
+}
+
+class HelperClass{
+    public void ArrayProcesser(int N, Scanner sc){
+        int[] arr = new int[N];
+        
+        // iterate using normal for loop
+        System.out.print("Enter " + N + " Values\n");
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        // print array using for each
+        System.out.print("Using for-each:\t");    
+        for(int x : arr){
+            System.out.print(x + " ");    
+        }
+        System.out.println();    
+
+        // print reverse in reverse
+        System.out.print("Reverse:\t");    
+        for(int i=N-1; i>=0; i--){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public void ArrayProcesser(int N, int M, Scanner sc){
+        int[][] mat = new int[N][M];   
+
+        System.out.print("Enter " + N*M + " Values\n");
+        
+        // iterate using normal for loop
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        
+        // print matrix using normal for loop
+        System.out.print("Given Matrix\n");
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                System.out.print(mat[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+    }
+    
+    public void ArrayProcesser(int N, int M, int P, Scanner sc){
+        int[][][] space = new int[N][M][P];   
+
+        System.out.print("Enter " + N*M*P + " Values\n");
+        // iterate using normal for loop
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                for(int k=0; k<P; k++){
+                    space[i][j][k] = sc.nextInt();
+                }
+            }
+        }
+
+        System.out.println();
+    }
+}
