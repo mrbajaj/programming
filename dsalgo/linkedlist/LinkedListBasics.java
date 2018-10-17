b2fc464ebef8c24844f015bfea8ee9a6c6044a63
mrbajaj <mayurkumarbajaj@gmail.com>
2018-10-17 23:54:45 +0530

    Change: rearranged java collections practice progras

--- dsalgo/linkedlist/LinkedListBasics.java
+++ dsalgo/linkedlist/LinkedListBasics.java
@@ -2,7 +2,42 @@ import java.lang.*;
 import java.util.*;
 import java.io.*;
 
-public class LinkedList{
+public class LinkedListBasics{
+    public static void main(String[] args){
+        LinkedList myList = new LinkedList();  
+
+        // myList.insertIntermediate(10, 0); myList.printList();
+        // myList.insertAtBeginning(9); myList.printList();   
+        // myList.insertAtBeginning(7); myList.printList();   
+        // myList.insertAtBeginning(5); myList.printList();   
+        // myList.insertAtEnd(11); myList.printList();   
+        // myList.insertAtEnd(13); myList.printList();   
+        // myList.insertAtEnd(14); myList.printList();   
+        // myList.insertIntermediate(18,10); myList.printList();
+        // myList.insertIntermediate(2, 0); myList.printList();
+        // myList.insertIntermediate(3, 1); myList.printList();
+        // myList.insertIntermediate(6, 4); myList.printList();   
+        // myList.insertIntermediate(12, 9); myList.printList();   
+        // myList.insertIntermediate(4, 3); myList.printList();
+
+        myList.insertSorted(10); myList.printList();
+        myList.insertSorted(9); myList.printList();   
+        myList.insertSorted(7); myList.printList();   
+        myList.insertSorted(5); myList.printList(); 
+        myList.insertSorted(11); myList.printList();   
+        myList.insertSorted(13); myList.printList();   
+        myList.insertSorted(14); myList.printList();
+        myList.insertSorted(18); myList.printList();
+        myList.insertSorted(2); myList.printList();
+        myList.insertSorted(3); myList.printList();
+        myList.insertSorted(6); myList.printList();   
+        myList.insertSorted(12); myList.printList();   
+        myList.insertSorted(4); myList.printList();
+    
+    }
+
+}
+class LinkedList{
     Node head;
     static class Node{
         int data;
@@ -87,40 +122,5 @@ public class LinkedList{
             this.insertIntermediate(value, pos+1);
         }
     }
-
-    public static void main(String[] args){
-        LinkedList myList = new LinkedList();  
-
-        // myList.insertIntermediate(10, 0); myList.printList();
-        // myList.insertAtBeginning(9); myList.printList();   
-        // myList.insertAtBeginning(7); myList.printList();   
-        // myList.insertAtBeginning(5); myList.printList();   
-        // myList.insertAtEnd(11); myList.printList();   
-        // myList.insertAtEnd(13); myList.printList();   
-        // myList.insertAtEnd(14); myList.printList();   
-        // myList.insertIntermediate(18,10); myList.printList();
-        // myList.insertIntermediate(2, 0); myList.printList();
-        // myList.insertIntermediate(3, 1); myList.printList();
-        // myList.insertIntermediate(6, 4); myList.printList();   
-        // myList.insertIntermediate(12, 9); myList.printList();   
-        // myList.insertIntermediate(4, 3); myList.printList();
-
-        myList.insertSorted(10); myList.printList();
-        myList.insertSorted(9); myList.printList();   
-        myList.insertSorted(7); myList.printList();   
-        myList.insertSorted(5); myList.printList(); 
-        myList.insertSorted(11); myList.printList();   
-        myList.insertSorted(13); myList.printList();   
-        myList.insertSorted(14); myList.printList();
-        myList.insertSorted(18); myList.printList();
-        myList.insertSorted(2); myList.printList();
-        myList.insertSorted(3); myList.printList();
-        myList.insertSorted(6); myList.printList();   
-        myList.insertSorted(12); myList.printList();   
-        myList.insertSorted(4); myList.printList();
-    
-    }
-
-
 }
 
---
+++ dsalgo/tree/TreeBasics.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class TreeBasics{
+
+}
+
+class Tree{
+    static class Node{
+        Node left;
+        int d;
+        Node right;
+
+        Node(int d){
+            this.d = d;
+            this.left = this.right = null;
+        }
+    }
+
+}
+
