e050190120fbd64e6df69b68a2f742d0884d801f
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2019-09-15 00:35:36 +0530

    Fix: queue issues fixed

--- queue/QueueDemo.java
+++ queue/QueueDemo.java
@@ -7,20 +7,27 @@ public class QueueDemo{
     public static void main(String[] args) {
         MyQueue queue = new MyQueue();
 
+        queue.dequeue();
+
         queue.enqueue(4); queue.print();
         queue.enqueue(7); queue.print();
-
         queue.enqueue(9); queue.print();
+        queue.enqueue(12); queue.print();
+        queue.enqueue(15); queue.print();
 
         queue.dequeue(); queue.print();
         
+        queue.enqueue(18); queue.print();
+
+        queue.dequeue(); queue.print();
+
+        queue.enqueue(21); queue.print();
+        
+        queue.enqueue(23); queue.print();
+
         // System.out.println("Peek: " + queue.peek());
-
-        // queue.enqueue(10); queue.print();
-
         // System.out.println("Index of 9: " + queue.search(9));
 
-        
     }
 }
 
@@ -29,15 +36,14 @@ public class QueueDemo{
  * @author Mayur Bajaj
  */
 class MyQueue{
-    private static final int MAX = 100;
-    private int size;
-    private int front, rear;
+    private static final int MAX = 5;
+    private int front, rear, size;
     private int[] a = new int[MAX];
     
     MyQueue(){
-        front = 0;
-        rear = -1;
-        size = 0;
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
     }
 
     /**
@@ -46,12 +52,12 @@ class MyQueue{
      */
     public void enqueue(int item){
         if( isFull() == true){
-            // overflow
-            System.out.println("Overflow");
+            System.out.println("Overflow: unable to insert " + item);
+            return;
         }
-        rear = (rear+1)%MAX;
-        a[rear] = item;
-        size++;
+        this.rear = (this.rear+1)%MAX;
+        this.a[this.rear] = item;
+        this.size++;
     }
 
 
@@ -60,13 +66,12 @@ class MyQueue{
      */
     public int dequeue(){
         if( isEmpty() == true){
-            // overflow
-            System.out.println("Underflow");
+            System.out.println("Underflow: Stack is empty, nothing to dequeue");
             return Integer.MIN_VALUE;
         }
-        int item = a[front];
-        front = (front+1)%MAX;
-        size--;
+        int item = a[this.front];
+        this.front = (this.front+1)%MAX;
+        this.size--;
         return item;
 
     }
@@ -76,7 +81,6 @@ class MyQueue{
      * Read the front element from the queue without removing it
      */
     public int peek(){
-        // ToDo
         if(isEmpty() == true){
             return Integer.MIN_VALUE;
         }
@@ -88,8 +92,7 @@ class MyQueue{
      * @return
      */
     public Boolean isEmpty(){
-        // ToDO
-        return (this.size == 0);
+       return (this.size == 0);
     }
 
 
@@ -97,25 +100,26 @@ class MyQueue{
      * Check the stack is full or not
      */
     public Boolean isFull(){
-        // ToDO
-        return (this.size == this.MAX);
+        return (this.size == MAX);
     }
 
     /**
      * Print the queue
      */
     public void print(){
-        if(isEmpty() == true){
+        if(this.isEmpty() == true){
             System.out.println("Stack is empty");
+            return;
         }
+        
+        int itr = this.front;
 
-        int itr = front;
-
-        while (itr != rear) {
+        while (itr != this.rear) {
             System.out.print(a[itr] + " ");
-            itr = (itr+1)%size;
+            itr = (itr+1) % MAX;
         }
-        System.out.println(a[itr]);
+        System.out.println(this.a[itr]);
+    
     }
 
     /**
