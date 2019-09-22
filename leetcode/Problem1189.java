ee0dded4805ad933ffb44dafe5b64831413f2209
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2019-09-22 21:26:26 +0530

    Solved: leetcode 1189

---
+++ leetcode/Problem1189.java
@@ -0,0 +1,37 @@
+/**
+ * URL: https://leetcode.com/problems/maximum-number-of-balloons/
+ */
+import java.util.Scanner;
+
+class Problem1189{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        Problem1189 obj = new Problem1189();
+
+        int count = obj.maxNumberOfBalloons(text.toLowerCase());
+
+        System.out.println(count);
+    }
+
+    public int maxNumberOfBalloons(String text) {
+        int[] charCount = new int[26];
+
+        for(int i=0; i < text.length(); i++){
+            int ascii = (int)text.charAt(i);
+            int index = ascii - 97;
+            charCount[index]++;
+        }
+        int count = 0;
+        while(charCount[1] >=1 && charCount[0] >=1 && charCount[11] >= 2  && charCount[14] >= 2  && charCount[13] >= 1){
+            charCount[1]--; //b
+            charCount[0]--; //a
+            charCount[11]--; charCount[11]--; //ll
+            charCount[14]--; charCount[14]--; //oo
+            charCount[13]--; //n
+            count++;   
+        }
+        return count;
+    }    
+}
