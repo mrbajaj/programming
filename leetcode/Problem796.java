42cc4452227907b913c790b992baebb370ec2954
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2019-12-25 11:19:23 +0530

    Solved: Easy, String

--- leetcode/Problem796.java
+++ leetcode/Problem796.java
@@ -1,18 +1,22 @@
 /**
  * URL:: https://leetcode.com/problems/rotate-string/
+ * 
  */
 class Problem796{
 
     public static void main(String[] args) {
-        String A = "abcde";
-        String B = "cdeab"; //"abced";
 
-        System.out.println("Ans: " + rotateString(A, B));
+        Problem796 obj = new Problem796();
+        
+        System.out.println("Ans: " + obj.rotateString("abcde", "cdeab"));
+
+        System.out.println("Ans: " + obj.rotateString("abcde", "abced"));
+
         
 
     }
 
-    public static boolean rotateString(String A, String B) {
+    public boolean rotateString(String A, String B) {
         if(A.length() != B.length()){
             return false;
         }
