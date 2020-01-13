9e309265a75fd270f97a00d902409db4abbd3fbf
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-01-13 21:10:31 +0530

    Solved: Medium, Tree, BST

--- leetcode/Problem230.java
+++ leetcode/Problem230.java
@@ -13,15 +13,22 @@ class Problem230{
         TreeNode left;
         TreeNode right;
         TreeNode(int x) { val = x; }
+
+        @Override
+        public String toString() {
+            return String.valueOf(this.val);
+        }
     }
     public static void main(String[] args) {
         Problem230 obj = new Problem230();
 
         TreeNode root = null;
         System.out.println("Ans: " + obj.kthSmallest(root, 1));
+        System.out.println("Opt   Ans: " + obj.kthSmallest_Optimized(root, 1));
 
         root = new TreeNode(1);
         System.out.println("Ans: " + obj.kthSmallest(root, 1));
+        System.out.println("Opt   Ans: " + obj.kthSmallest_Optimized(root, 1));
         
         root = new TreeNode(10);
         root.left = new TreeNode(3);
@@ -30,7 +37,8 @@ class Problem230{
         root.left.right = new TreeNode(8);
         root.left.right.left = new TreeNode(7);
         root.right.right = new TreeNode(20);
-        System.out.println("Ans: " + obj.kthSmallest(root, 2));
+        System.out.println("Basic Ans: " + obj.kthSmallest(root, 4));
+        System.out.println("Opt   Ans: " + obj.kthSmallest_Optimized(root, 4));
 
     }
 
@@ -56,4 +64,45 @@ class Problem230{
         list.add(root.val);
         kthSmallestUtil(root.right, list);
     }
+
+    public int kthSmallest_Optimized(TreeNode root, int k) {
+        int c = 0;
+        if (root == null) 
+            return 0 ; 
+  
+  
+        java.util.Stack<TreeNode> s = new java.util.Stack<TreeNode>(); 
+        TreeNode curr = root; 
+  
+        while (curr != null || s.size() > 0) 
+        { 
+            
+
+            // go to extreme left from curr node
+            while (curr !=  null) 
+            { 
+                s.push(curr); 
+                curr = curr.left; 
+            }
+
+            // pop out the top node from stack
+            curr = s.pop(); 
+  
+            //process the node
+            c++;
+            if(c == k){
+                return curr.val;
+            }
+  
+            // jump one step right
+            curr = curr.right; 
+
+        }
+
+        return 0;
+    }   
+
+    
+    
+
 }
