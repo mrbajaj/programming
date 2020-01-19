a2e71daba265960a4d0d2284ffff967fe6afdf4d
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-01-19 17:34:55 +0530

    Solved: Easy, Recursion

--- leetcode/Problem509.java
+++ leetcode/Problem509.java
@@ -15,12 +15,26 @@ class Problem509{
 
     }
 
-    public int fib(int N) {
+    public int fibRec(int N) {
         if(N <= 1){
             return N;
         } else{
             return fib(N-1) + fib(N-2);
         }
+    }
+
+    public int fib(int n){
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        
+        for(int i = 2; i <= n; i++){
+            arr[i] = arr[i-2] + arr[i-1];
+        }
+        return arr[n];
     }
 
 }
