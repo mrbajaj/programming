149be0645e11da41968c4c85a4c43b0163ada28e
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-02-09 12:25:21 +0530

    Solved: Easy, Math

---
+++ leetcode/Problem1342.java
@@ -0,0 +1,27 @@
+
+class Problem1342{
+
+    public static void main(String[] args) {
+        Problem1342 obj = new Problem1342();
+
+        System.out.println("Ans: " + obj.numberOfSteps(14));
+
+    }
+
+    public int numberOfSteps(int n) {
+        int count = 0;
+
+        while(n != 0){
+
+            if(n % 2 == 0){
+                n = n/2;
+            }else{
+                n--;
+            }
+            count++;
+        }
+
+
+        return count;
+    }
+}
