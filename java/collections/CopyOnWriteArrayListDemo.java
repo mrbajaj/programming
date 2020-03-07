55dd10787985683ff5c6594584033900273a21b1
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-03-07 15:58:01 +0530

    Add CopyOnWriteArrayList Demo

---
+++ java/collections/CopyOnWriteArrayListDemo.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+// import java.util.ArrayList;
+
+class CopyOnWriteArrayListDemo implements Runnable{
+
+    static CopyOnWriteArrayList<String> cList = new CopyOnWriteArrayList<>();
+
+    // static ArrayList<String> cList = new ArrayList<>();  // it will throw ConcurrentModificationException
+
+    @Override
+    public void run() {
+        try{Thread.sleep(200);}catch(Exception e){}
+        cList.add("Chandigarh");
+    }
+    public static void main(String[] args) {
+        CopyOnWriteArrayListDemo obj = new CopyOnWriteArrayListDemo();
+
+        obj.build();
+
+        Thread t = new Thread(obj);
+        t.start();
+
+        Iterator<String> i = cList.iterator();
+        
+        while(i.hasNext()){
+            System.out.println(i.next());
+            try{Thread.sleep(300);}catch(Exception e){}
+        }
+
+        while(t.isAlive()){
+            
+        }
+
+        System.out.println(cList);
+    }
+
+
+    public void build(){
+        
+        cList.add("Pune");
+        cList.add("Bombay");
+        cList.add("Kolkata");
+        cList.add("Chennai");
+        cList.add("Delhi");
+
+        cList.addIfAbsent("Pune");
+
+        cList.addAllAbsent(Arrays.asList("Indore", "Jaipur", "Pune") );
+
+    }
+
+
+}
