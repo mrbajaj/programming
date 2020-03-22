0341340e7b8d517abcebf912b69a404c085c1d2d
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-03-22 22:57:38 +0530

    Resolved: Easy, Tree

--- leetcode/Problem104.java
+++ leetcode/Problem104.java
@@ -1,8 +1,9 @@
 /**
- * URL: https://leetcode.com/problems/same-tree/
+ * URL: 
  * 
  */
-class Problem104{
+
+class Problem104 {
 
     public static class TreeNode {
         int val;
@@ -18,34 +19,36 @@ class Problem104{
 
     public static void main(String[] args) {
         Problem104 obj = new Problem104();
-       
-        // Test1
-        TreeNode p = new TreeNode(1);
-        p.left = new TreeNode(2);
-        p.right = new TreeNode(3);
-        
-        System.out.println("Ans: " + obj.maxDepth(p));
 
-        // Test2
-         p = new TreeNode(1);
-        p = new TreeNode(4);  
+        TreeNode p = new TreeNode(4);  
         p.left = new TreeNode(2);  
         p.right = new TreeNode(9);  
         p.left.left = new TreeNode(3);  
         p.left.right = new TreeNode(8);  
         p.right.right = new TreeNode(7);
-        
-        
-        System.out.println("Ans: " + obj.maxDepth(p));
+
+        obj.printTree(p); System.out.println();
+        System.out.println("Ans: " + obj.maxDepth(p) + "\n");
     }
-    
+
     public int maxDepth(TreeNode root) {
         if(root == null){
             return 0;
         }
-        int ld = maxDepth(root.left);
-        int rd = maxDepth(root.right);
-        return Math.max(ld,rd) + 1;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
+
+    
+
+    public void printTree(TreeNode root) {
+        
+        if(root == null){
+            return;
+        }
+        printTree(root.left);
+        System.out.print(root.val + " ");
+        printTree(root.right);
+        
     }
 
 }
