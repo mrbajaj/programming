e8674bee78b148b7df1c24acccbb3c05d3bbd7ec
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-03-24 01:29:59 +0530

    Resolved: Easy, Array

--- leetcode/Problem26.java
+++ leetcode/Problem26.java
@@ -9,26 +9,28 @@ class Problem26{
         Problem26 obj = new Problem26();
 
         System.out.println("Ans: " + obj.removeDuplicates(new int[]{1,1,2}));
+        
+        System.out.println("Ans: " + obj.removeDuplicates(new int[]{0,1,2,3,4,5}));
 
         System.out.println("Ans: " + obj.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
     }
 
     public int removeDuplicates(int[] nums) {
-        int n = nums.length;
-        int j=0;
-        for(int i=0; i < n; i++){
-            int curr = nums[i];
-            while( i < n && nums[i] == curr ){
-                // nums[i] = 0;
+        if(nums.length == 0){
+            return 0;
+        }
+        
+        int length = -1;
+        for(int i = 0 ; i < nums.length;){
+            int currItem = nums[i];
+            while( i < nums.length && currItem == nums[i] ){
                 i++;
             }
-            i--;
-
-            nums[j] = curr;
-            j++;
-
+            length++;
+            nums[length] = currItem;
         }
-        System.out.println(Arrays.toString(nums));
-        return j;
+
+        return length+1;
+
     }
 }
