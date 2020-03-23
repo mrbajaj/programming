151ca5f86bf6ae72262966376f8881accbdba604
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-03-24 01:38:22 +0530

    ReSolved: Easy, Array

--- leetcode/Problem26.java
+++ leetcode/Problem26.java
@@ -3,19 +3,25 @@ import java.util.Arrays;
 /**
  * URL: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
  * 
+ * Also solved for not given sorted array
+ * 
  */
 class Problem26{
     public static void main(String[] args) {
         Problem26 obj = new Problem26();
 
+        System.out.println("Ans: " + obj.removeDuplicates(new int[]{1,2,1}));
         System.out.println("Ans: " + obj.removeDuplicates(new int[]{1,1,2}));
         
         System.out.println("Ans: " + obj.removeDuplicates(new int[]{0,1,2,3,4,5}));
+        System.out.println("Ans: " + obj.removeDuplicates(new int[]{0,3,4,5,1,2}));
 
+        System.out.println("Ans: " + obj.removeDuplicates(new int[]{1,1,2,3,3,4,1,2,0,0}));
         System.out.println("Ans: " + obj.removeDuplicates(new int[]{0,0,1,1,1,2,2,3,3,4}));
     }
 
     public int removeDuplicates(int[] nums) {
+        Arrays.sort(nums);
         if(nums.length == 0){
             return 0;
         }
