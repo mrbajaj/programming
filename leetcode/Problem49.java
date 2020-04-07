b2320cc61084ffa41f5911e39ede3b223fa6f558
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-04-07 23:53:40 +0530

    ReSolved: Medium, String

--- leetcode/Problem49.java
+++ leetcode/Problem49.java
@@ -3,85 +3,159 @@
  * 
  */
 import java.util.List;
-import java.util.Map;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-class Problem49{
+public class Problem49{
     public static void main(String[] args) {
-        Problem49 obj = new Problem49();
+        Solution1 sol1 = new Solution1();
+        Solution2 sol2 = new Solution2();
+        Solution3 sol3 = new Solution3();
         
-        List<List<String>> ansSorting = obj.groupAnagrams_sorting(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
-        List<List<String>> ansHashing = obj.groupAnagrams_hashing(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
-        
+        List<List<String>> ansSorting = sol1.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
         System.out.println(ansSorting);
+        
+        List<List<String>> ansCustomKey = sol2.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
+        System.out.println(ansCustomKey);
+
+        List<List<String>> ansHashing = sol3.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"});
         System.out.println(ansHashing);
 
     }
-    
-    public List<List<String>> groupAnagrams_hashing(String[] strs) {
-        List<List<String>> ans = new ArrayList<List<String>>();
-
-        Map<Integer, ArrayList<String>> map = new HashMap<>();
-
-        for(String str : strs){
-            int[] freq = new int[26];
-            for(int i = 0; i < str.length(); i++){
-                freq[str.charAt(i) - 'a'] += 1;
-            }
-
-            int hashcode = Arrays.hashCode(freq);
-
-            if(map.containsKey(hashcode)){
-                ArrayList<String> grouped = map.get(hashcode);
-                grouped.add(str);
-                map.put(hashcode, grouped);
-            }else{
-                ArrayList<String> grouped = new ArrayList<>();
-                grouped.add(str);
-                map.put(hashcode, grouped);
-            }
-        }
-
-        Set<Integer> keys = map.keySet();
-        for(Integer key :  keys){
-            ans.add(map.get(key));
-        }
-
-        return ans;
-    }
-
-
-    public List<List<String>> groupAnagrams_sorting(String[] strs) {
-        List<List<String>> ans = new ArrayList<List<String>>();
-
-        Map<String, ArrayList<String>> map = new HashMap<>();
-
-        for(String str : strs){
-            
-            char[] chars = str.toCharArray();
-            Arrays.sort(chars);
-            String sorted = new String(chars);
-
-            if(map.containsKey(sorted)){
-                ArrayList<String> grouped = map.get(sorted);
-                grouped.add(str);
-                map.put(sorted, grouped);
-            }else{
-                ArrayList<String> grouped = new ArrayList<>();
-                grouped.add(str);
-                map.put(sorted, grouped);
-            }
-        }
-
-        Set<String> keys = map.keySet();
-
-        for(String key :  keys){
-            ans.add(map.get(key));
-        }
-
-        return ans;
-    }
 }
+
+
+class Solution1 {
+    //sorting
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+
+        Map<String, List<String>> groups = new HashMap<>();
+
+        for(String str : strs){
+            String key = this.sortString(str);
+            List<String> list = groups.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            groups.put(key,list);
+        }
+
+        Set<String> keys = groups.keySet();
+
+        for(String key: keys){
+            result.add(groups.get(key));
+        }
+
+        return result;
+
+    }
+
+    public String sortString(String inputString) 
+    { 
+        // convert input string to char array 
+        char tempArray[] = inputString.toCharArray(); 
+          
+        // sort tempArray 
+        Arrays.sort(tempArray); 
+          
+        // return new sorted string 
+        return new String(tempArray); 
+    } 
+}
+
+class Solution2{
+    //CustomKey
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+
+        Map<String, List<String>> groups = new HashMap<>();
+
+        for(String str : strs){
+            String key = this.customKey(str);
+            List<String> list = groups.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            groups.put(key,list);
+        }
+
+        Set<String> keys = groups.keySet();
+
+        for(String key: keys){
+            result.add(groups.get(key));
+        }
+
+        return result;
+    }
+
+    public String customKey(String inputString){
+        int[] freq = new int[26];
+
+        for(int i=0; i<inputString.length(); i++){
+            int index = inputString.charAt(i) - 'a';
+            freq[index]++;
+        }
+
+        return Arrays.stream(freq)
+        .mapToObj(String::valueOf)
+        .collect(Collectors.joining("#"));
+
+    }
+
+}
+
+class Solution3{
+    //hashing : TLE
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<List<String>>();
+        List<Integer> covered = new ArrayList<>();
+
+        for(int i = 0; i < strs.length; i++){
+            if(covered.contains(i)){
+                continue;
+            }
+            String str = strs[i];
+            List<String> currList = new ArrayList<String>();
+            currList.add(str);
+            covered.add(i);
+            for(int j = i+1; j< strs.length; j++){
+                if(!covered.contains(j) && this.isAnagram(str, strs[j])){
+                    currList.add(strs[j]);
+                    covered.add(j);
+                }
+            }
+            result.add(currList);
+        }
+
+        return result;
+    }
+
+    public boolean isAnagram(String str1, String str2){
+        if(str1.length() != str2.length()){
+            return false;
+        }
+        
+        int[] freq = new int[26];
+
+        for(int i=0; i<str1.length(); i++){
+            int index = str1.charAt(i) - 'a';
+            freq[index]++;
+        }
+
+        for(int i=0; i<str1.length(); i++){
+            int index = str2.charAt(i) - 'a';
+            freq[index]--;
+        }
+
+        for(int i=0; i< freq.length; i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+}
+
