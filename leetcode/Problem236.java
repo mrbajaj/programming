03b68721a7562d9d45299dd90b8c876b47e55924
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-04-11 19:33:37 +0530

    ReSolved, Medium, Tree

--- leetcode/Problem236.java
+++ leetcode/Problem236.java
@@ -3,25 +3,30 @@
  * 
  * 
  */
-class Problem236{
-    
-    static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-        TreeNode(int x) { val = x; }
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
 
-        @Override
-        public String toString() {
-            return String.valueOf(this.val);
-        }
+    @Override
+    public String toString() {
+        return String.valueOf(this.val);
     }
+}
+class Problem236{
+
     public static void main(String[] args) {
-        Problem236 obj = new Problem236();
+        Solution sol = new Solution();
+
+        TreeNode p1 = new TreeNode(5);
+        TreeNode p2 = new TreeNode(1);
+        TreeNode p3 = new TreeNode(4);
+        TreeNode p4 = new TreeNode(10);
 
         TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(5);
-        root.right = new TreeNode(1);
+        root.left = p1;
+        root.right = p2;
         
         root.left.left = new TreeNode(6);
         root.left.right = new TreeNode(2);
@@ -30,32 +35,29 @@ class Problem236{
         root.right.right = new TreeNode(8);
 
         root.left.right.left = new TreeNode(7);
-        root.left.right.right = new TreeNode(4);
+        root.left.right.right = p3;
         
-        System.out.println("Ans: " + obj.lowestCommonAncestor(root, new TreeNode(5), new TreeNode(1)));
-        System.out.println("Ans: " + obj.lowestCommonAncestor(root, new TreeNode(5), new TreeNode(4)));
-        System.out.println("Ans: " + obj.lowestCommonAncestor(root, new TreeNode(5), new TreeNode(10)));
+        System.out.println("Ans: " + sol.lowestCommonAncestor(root, p1, p2) );
+        System.out.println("Ans: " + sol.lowestCommonAncestor(root, p1, p3) );
+        System.out.println("Ans: " + sol.lowestCommonAncestor(root, p1, p4) );
         
     }
+}
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+class Solution{
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
         if(root == null){
             return null;
         }
 
-        if(root.val == p.val){
-            return p;
-        }
-
-        if(root.val == q.val){
-            return q;
+        if(root == p || root == q){
+            return root;
         }
 
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
 
         if(left != null && right != null){
-            // found the lca
             return root;
         }
 
@@ -66,7 +68,6 @@ class Problem236{
         if(right != null){
             return right;
         }
-
 
         return null;
     }
