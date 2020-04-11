4ccb20272c6f46c2fbddf97d90e2bf102b82434a
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-04-11 19:01:46 +0530

    ReSolved: Easy, Math

--- leetcode/Problem1342.java
+++ leetcode/Problem1342.java
@@ -6,13 +6,22 @@
 class Problem1342{
 
     public static void main(String[] args) {
-        Problem1342 obj = new Problem1342();
+        Solution1 sol1 = new Solution1();
+        Solution2 sol2 = new Solution2();
+        Solution3 sol3 = new Solution3();
 
-        System.out.println("Ans: " + obj.numberOfSteps(14));
+        System.out.println("Ans: " + sol1.numberOfSteps(14));
+        System.out.println("Ans: " + sol2.numberOfSteps(14));
+        System.out.println("Ans: " + sol3.numberOfSteps(14));
 
     }
 
+    
+}
+
+class Solution1{
     public int numberOfSteps(int n) {
+        // iterative
         int count = 0;
 
         while(n != 0){
@@ -27,5 +36,27 @@ class Problem1342{
 
 
         return count;
+    }
+}
+
+class Solution2{
+    // recursive
+    public int numberOfSteps(int n) {
+        if(n == 0){
+            return 0;
+        }
+        if(n % 2 == 0){
+            return 1 + numberOfSteps(n/2);
+        }else{
+            return 1 + numberOfSteps(n-1);
+        }
+    }
+}
+
+class Solution3{
+    // bit manipulation
+    public int numberOfSteps(int n) {
+        // TODO
+        return 0;
     }
 }
