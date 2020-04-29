34860a3c2847c27753076f3ec782715c60a866c4
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-04-30 00:06:46 +0530

    ReSolved: Easy, Math

--- leetcode/Problem168.java
+++ leetcode/Problem168.java
@@ -5,32 +5,30 @@
 class Problem168{
 
     public static void main(String[] args) {
-        Problem168 obj = new Problem168();
-        System.out.println("Ans: " + obj.convertToTitle(1) + "\n"); // A
-        System.out.println("Ans: " + obj.convertToTitle(3) + "\n"); // C
-        System.out.println("Ans: " + obj.convertToTitle(26) + "\n"); // Z
-        System.out.println("Ans: " + obj.convertToTitle(28) + "\n"); // AB
-        System.out.println("Ans: " + obj.convertToTitle(52) + "\n"); // AZ
-        System.out.println("Ans: " + obj.convertToTitle(701) + "\n"); // ZY
-        System.out.println("Ans: " + obj.convertToTitle(1200) + "\n"); // ATD
+        Solution obj = new Solution();
+        System.out.println("Ans: A | " + obj.convertToTitle(1) + "\n"); // A
+        System.out.println("Ans: C | " + obj.convertToTitle(3) + "\n"); // C
+        System.out.println("Ans: Z | " + obj.convertToTitle(26) + "\n"); // Z
+        System.out.println("Ans: AB | " + obj.convertToTitle(28) + "\n"); // AB
+        System.out.println("Ans: AZ | " + obj.convertToTitle(52) + "\n"); // AZ
+        System.out.println("Ans: ZY | " + obj.convertToTitle(701) + "\n"); // ZY
+        System.out.println("Ans: ATD | " + obj.convertToTitle(1200) + "\n"); // ATD
     }
 
+  
+            
+}
+
+class Solution {
     public String convertToTitle(int n) {
         StringBuilder sb = new StringBuilder();
-
-        System.out.println("n:" + n);
-
         while(n > 0){
             n--;
-            
-            int rem = n%26;
-            System.out.println("n:" + n + "\t rem:" + rem);
-            sb.insert(0, (char) (rem+65));
-
+            int r = n % 26;
+            sb.insert(0, (char) (r+65));
             n /= 26;
         }
-
+       
         return sb.toString();
     }
-            
 }
