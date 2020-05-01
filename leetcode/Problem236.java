27c199a2d73f74f3b9b66dd499f3f75eef07eb67
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-05-02 00:17:14 +0530

    ReSolved: Medium, Tree

--- leetcode/Problem236.java
+++ leetcode/Problem236.java
@@ -1,52 +1,82 @@
 /**
- * URL: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
- * 
+ * URL: 
  * 
  */
 class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode(int x) { val = x; }
 
-    @Override
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+    
     public String toString() {
         return String.valueOf(this.val);
     }
 }
-class Problem236{
+
+class Problem236 {
 
     public static void main(String[] args) {
+        Problem236 obj = new Problem236();
         Solution sol = new Solution();
 
-        TreeNode p1 = new TreeNode(5);
-        TreeNode p2 = new TreeNode(1);
-        TreeNode p3 = new TreeNode(4);
-        TreeNode p4 = new TreeNode(10);
+        TreeNode a = new TreeNode(10);
+        TreeNode b = new TreeNode(20);
+        TreeNode p = new TreeNode(5);
+        p.left = a;
+        p.right = b;
+        obj.printTree(p); System.out.println();
+        System.out.println("Ans : " + sol.lowestCommonAncestor(p, a, b) + "\n");
 
-        TreeNode root = new TreeNode(3);
-        root.left = p1;
-        root.right = p2;
-        
-        root.left.left = new TreeNode(6);
-        root.left.right = new TreeNode(2);
-        
-        root.right.left = new TreeNode(0);
-        root.right.right = new TreeNode(8);
+        a = new TreeNode(10);
+        b = new TreeNode(20);
+        p = a;
+        p.left = b;
+        p.right = new TreeNode(9);  
+        obj.printTree(p); System.out.println();
+        System.out.println("Ans : " + sol.lowestCommonAncestor(p, a, b) + "\n");
 
-        root.left.right.left = new TreeNode(7);
-        root.left.right.right = p3;
+        a = new TreeNode(10);
+        b = new TreeNode(20);
+        p = a;
+        p.left = new TreeNode(9);
+        p.right = b;
+        obj.printTree(p); System.out.println();
+        System.out.println("Ans : " + sol.lowestCommonAncestor(p, a, b) + "\n");
+
+        a = new TreeNode(10);
+        b = new TreeNode(20);
+        p = new TreeNode(4);  
+        p.left = new TreeNode(7);
+        p.right = new TreeNode(9);  
+        p.left.left = a;
+        p.left.right = new TreeNode(8);  
+        p.right.right = b;
+        obj.printTree(p); System.out.println();
+        System.out.println("Ans : " + sol.lowestCommonAncestor(p, a, b) + "\n");
+    }
+
+    public void printTree(TreeNode root) {
         
-        System.out.println("Ans: " + sol.lowestCommonAncestor(root, p1, p2) );
-        System.out.println("Ans: " + sol.lowestCommonAncestor(root, p1, p3) );
-        System.out.println("Ans: " + sol.lowestCommonAncestor(root, p1, p4) );
+        if(root == null){
+            return;
+        }
+        printTree(root.left);
+        System.out.print(root.val + " ");
+        printTree(root.right);
         
     }
+
 }
 
-class Solution{
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q){
-        if(root == null){
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root  == null){
             return null;
         }
 
