dd4591fb71e69bdabbf05707dca5e00031f06828
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-05-02 16:44:45 +0530

    ReSolved: Medium, Tree, Stack

--- leetcode/Problem103.java
+++ leetcode/Problem103.java
@@ -9,33 +9,38 @@ import java.util.Collections;
  * URL: https://leetcode.com/problems/binary-tree-level-order-traversal/
  * 
  */
-class Problem103{
+import java.util.Stack;
 
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-        TreeNode(int x) {
-            val = x;
-            left = null;
-            right = null;
-        }
-
-        @Override
-        public String toString() {
-            return String.valueOf(this.val);
-        }
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
     }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.val);
+    }
+}
+
+public class Problem103{
+    
     public static void main(String[] args) {
-        Problem103 obj = new Problem103();
+        Solution1 sol1 = new Solution1();
+        Solution2 sol2 = new Solution2();
 
         TreeNode p = new TreeNode(1);
         p.left = new TreeNode(2);
         p.right = new TreeNode(4);
         p.right.left = new TreeNode(3);
 
-        System.out.println("Ans: " + obj.zigzagLevelOrder(p) + "\n\n\n");
+        System.out.println("Ans: " + sol1.zigzagLevelOrder(p));
+        System.out.println("Ans: " + sol2.zigzagLevelOrder(p) + "\n\n\n");
 
 
         p = new TreeNode(4);  
@@ -45,11 +50,68 @@ class Problem103{
         p.left.right = new TreeNode(8);  
         p.right.right = new TreeNode(7);
 
-        System.out.println("Ans: " + obj.zigzagLevelOrder(p));
+        System.out.println("Ans: " + sol1.zigzagLevelOrder(p));
+        System.out.println("Ans: " + sol2.zigzagLevelOrder(p) + "\n\n\n");
         
 
     }
 
+}
+class Solution1{
+    // using Stack
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> allList = new ArrayList<List<Integer>>();
+        if(root == null){
+            return allList;
+        }
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+
+        s1.push(root);
+        boolean isForward = true;
+        while(!s1.isEmpty()){
+            List<Integer> currList = new ArrayList<>();
+            if(isForward == true){
+                this.copyStack(s1,s2);
+                this.printAndCopyStack(s2,s1, currList);
+            }else{
+                this.printAndCopyStack(s1, s2, currList);
+                this.copyStack(s2,s1);
+            }
+            while(!s1.isEmpty()){
+                TreeNode node = s1.pop();
+                if(node.right != null){
+                    s2.push(node.right);
+                }
+                if(node.left != null){
+                    s2.push(node.left);
+                }
+            }
+            allList.add(currList);
+            this.copyStack(s2, s1);
+            isForward = !isForward;
+        }
+
+        return allList;
+    }
+
+    private void copyStack(Stack<TreeNode> source, Stack<TreeNode> dest){
+        while(!source.isEmpty()){
+            dest.push(source.pop());
+        }
+    }
+
+    private void printAndCopyStack(Stack<TreeNode> source, Stack<TreeNode> dest, List<Integer> currList){
+        while(!source.isEmpty()){
+            TreeNode node = source.pop();
+            currList.add(node.val);
+            dest.push(node);
+        }
+    }
+    
+}
+class Solution2{
+    // using Queue
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> allList = new ArrayList<List<Integer>>();
         TreeNode dummy = new TreeNode(0), prev = null;
@@ -91,5 +153,4 @@ class Problem103{
 
         return allList;
     }
-
 }
