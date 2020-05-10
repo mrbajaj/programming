bbe1e629e2487afc129a7d3a6712f5ff4827c916
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-05-10 19:58:07 +0530

    ReSolved: Medium, Tree

--- leetcode/Problem144.java
+++ leetcode/Problem144.java
@@ -6,41 +6,63 @@
  import java.util.ArrayList;
  import java.util.List;
 
-class Problem144 {
+ import java.util.Set;
+ import java.util.HashSet;
 
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
+ import java.util.Stack;
 
-        TreeNode(int x) {
-            val = x;
-            left = null;
-            right = null;
-        }
+ class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
     }
+}
 
+
+class Problem144 {
     public static void main(String[] args) {
         Problem144 obj = new Problem144();
+        Solution2 sol = new Solution2();
 
         TreeNode p = new TreeNode(1);
         p.right = new TreeNode(2);
         p.right.left = new TreeNode(3);
 
         obj.printTree(p); System.out.println();
-        System.out.println("Ans: " + obj.preorderTraversal(p) + "\n");
+        System.out.println("Ans: " + sol.preorderTraversal(p) + "\n");
 
-        p = new TreeNode(4);  
-        p.left = new TreeNode(2);  
-        p.right = new TreeNode(9);  
-        p.left.left = new TreeNode(3);  
-        p.left.right = new TreeNode(8);  
+        p = new TreeNode(1);
+        p.left = new TreeNode(2);
+        p.right = new TreeNode(5);
+        p.left.left = new TreeNode(3);
+        p.left.right = new TreeNode(4);
+        p.right.left = new TreeNode(6);
         p.right.right = new TreeNode(7);
 
         obj.printTree(p); System.out.println();
-        System.out.println("Ans: " + obj.preorderTraversal(p) + "\n");
+        System.out.println("Ans: " + sol.preorderTraversal(p) + "\n");
     }
 
+    public void printTree(TreeNode root) {
+        
+        if(root == null){
+            return;
+        }
+        printTree(root.left);
+        System.out.print(root.val + " ");
+        printTree(root.right);
+        
+    }
+
+}
+
+class Solution1 {
+    // Recursive
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ans = new ArrayList<>();
         this.preorderTraversal_util(root, ans );
@@ -55,16 +77,34 @@ class Problem144 {
         this.preorderTraversal_util(root.left, ans );
         this.preorderTraversal_util(root.right, ans );
     }
+}
 
-    public void printTree(TreeNode root) {
-        
+class Solution2 {
+    // Iterative
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
         if(root == null){
-            return;
+            return ans;
         }
-        printTree(root.left);
-        System.out.print(root.val + " ");
-        printTree(root.right);
         
-    }
+        Stack<TreeNode> stack = new Stack<>();
 
+        stack.add(root);
+        while(!stack.isEmpty()){
+            TreeNode curr = stack.pop();
+            System.out.print(curr.val + " ");
+            ans.add(curr.val);
+            if(curr.right !=  null){
+                stack.push(curr.right);
+            }
+            if(curr.left != null){
+                stack.push(curr.left);
+            }
+
+        }
+        
+        System.out.println();
+
+        return ans;
+    }
 }
