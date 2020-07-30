4ab0bb844b61ff0b535bc3725f92a404088ee0df
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-07-30 08:54:02 +0530

    ReSolved: Easy, LinkedList

--- leetcode/Problem876.java
+++ leetcode/Problem876.java
@@ -1,34 +1,82 @@
+/**
+ * 
+ * 
+ */
 
 public class Problem876{
-    
-    static class ListNode {
-        int val;
-        ListNode next;
-        ListNode(int x) { val = x; }
-    }
-
     public static void main(String[] args) {
-        ListNode head = new ListNode(5);
-        head.next = new ListNode(7);
-        head.next.next = new ListNode(9);
-        head.next.next.next = new ListNode(10);
-        // head.next.next.next.next = new ListNode(12);
-       
+        Problem876 obj = new Problem876();
 
-        System.out.println("Ans: " + middleNode(head).val);
+        ListNode a = new ListNode(3);
+        a.next = new ListNode(7);
+        a.next.next = new ListNode(8);
+        a.next.next.next = new ListNode(10);
 
+        ListNode b = new ListNode(99);
+        b.next = new ListNode(1);
+        b.next.next = new ListNode(4);
+        b.next.next.next = a.next.next;
+
+        obj.printList(a);
+        obj.printList(b);
+
+        Solution sol = new Solution();
+
+        System.out.println("Ans:" + sol.middleNode(a)  + "\n");
+        System.out.println("Ans:" + sol.middleNode(b)  + "\n");
+
+
+        
     }
 
-    static public ListNode middleNode(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
+    public void printList(ListNode head){
+        ListNode itr = head;
+        while(itr!=null){
+            System.out.print(itr +" ");
+            itr = itr.next;
+        }
+        System.out.println();
+    }
 
+    public int lengthOfList(ListNode head){
+        int length = 0;
+        ListNode itr = head;
+        while(itr!=null){
+            length++;
+            itr = itr.next;
+        }
+        return length;
+        
+    }
+
+}
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head == null){
+            return null;
+        }
+
+        ListNode fast = head, slow = head;
         while(fast != null && fast.next != null){
             fast = fast.next.next;
             slow = slow.next;
         }
 
         return slow;
+
     }
 }
 
+class ListNode{
+    Integer val;
+    ListNode next;
+    ListNode(int v){
+        this.val = v;
+        this.next = null;
+    }
+    @Override
+    public String toString() {
+        return String.valueOf(this.val);
+    }
+}
