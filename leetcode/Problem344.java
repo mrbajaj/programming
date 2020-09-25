1a492564c24f231ea43341b7b21e8d1ea7a873e1
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2020-09-25 22:38:48 +0530

    Sept2020: Easy, String

--- leetcode/Problem344.java
+++ leetcode/Problem344.java
@@ -13,17 +13,17 @@ class Problem344{
         char[] s = new char[]{'a','b','c','d','H'};
         obj.reverseString( s );
         System.out.println("Ans: " +  Arrays.toString(s));
-
-
         
     }
 
     public void reverseString(char[] s) {
-        int i = 0, j = s.length - 1;   
-        while (i < j){
-            char tmp = s[i];
+        
+        int i = 0, j = s.length -1;
+
+        while(i < j){
+            char t = s[i];
             s[i] = s[j];
-            s[j] = tmp;
+            s[j] = t;
             i++;
             j--;
         }
