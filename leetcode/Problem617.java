61c4c058c1b8e9ef36b6ddb1a5eaa81d0dc8719d
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-01-06 00:54:49 +0530

    Solved: Easy, Tree

---
+++ leetcode/Problem617.java
@@ -0,0 +1,85 @@
+/**
+ * URL: https://leetcode.com/problems/merge-two-binary-trees/
+ * Tags: Tree
+ * 
+ * Best Time Complexity:
+ * Best Space Complexity:
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+        left = null;
+        right = null;
+    }
+
+    public String toString() {
+        return String.valueOf(this.val);
+    }
+}
+
+public class Problem617 {
+
+    public static void main(String[] args) {
+        Problem617 obj = new Problem617();
+        Solution sol = new Solution();
+
+        TreeNode p = new TreeNode(1);
+        p.left = new TreeNode(3);  
+        p.right = new TreeNode(2);  
+        p.left.left = new TreeNode(5);  
+        
+        obj.printTree(p); System.out.println();
+
+        TreeNode q = new TreeNode(2);  
+        q.left = new TreeNode(1);  
+        q.right = new TreeNode(3);  
+        q.left.right = new TreeNode(4);  
+        q.right.right = new TreeNode(7);
+
+        obj.printTree(p); System.out.println();
+
+        System.out.println("Ans: \n");
+        TreeNode r = sol.mergeTrees(p, q);
+        obj.printTree(r); System.out.println();
+    }
+
+    public void printTree(TreeNode root) {
+        
+        if(root == null){
+            return;
+        }
+        printTree(root.left);
+        System.out.print(root.val + " ");
+        printTree(root.right);
+        
+    }
+
+}
+
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null){
+            return null;
+        }
+
+        TreeNode newNode;
+        if(t1 != null && t2 != null){
+            newNode = new TreeNode(t1.val + t2.val);
+            newNode.left = mergeTrees(t1.left, t2.left);
+            newNode.right = mergeTrees(t1.right, t2.right);    
+        }else if(t1 != null){
+            newNode = t1;
+            newNode.left = mergeTrees(t1.left, null);
+            newNode.right = mergeTrees(t1.right, null);
+        }else{
+            newNode = t2;
+            newNode.left = mergeTrees(null, t2.left);
+            newNode.right = mergeTrees(null, t2.right);
+        }
+        return newNode;
+    }
+}
