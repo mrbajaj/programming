a6f332ed3b391b141316a8764da0a7119233d6ea
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-01-06 00:59:14 +0530

    Solved: Easy, Tree

--- leetcode/Problem617.java
+++ leetcode/Problem617.java
@@ -25,7 +25,7 @@ public class Problem617 {
 
     public static void main(String[] args) {
         Problem617 obj = new Problem617();
-        Solution sol = new Solution();
+        Solution2 sol = new Solution2();
 
         TreeNode p = new TreeNode(1);
         p.left = new TreeNode(3);  
@@ -81,5 +81,28 @@ class Solution {
             newNode.right = mergeTrees(null, t2.right);
         }
         return newNode;
+    }
+}
+
+class Solution2{
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null){
+            return null;
+        }
+
+        if(t1 != null && t2 != null){
+            t1.val = t1.val + t2.val;
+            t1.left = mergeTrees(t1.left, t2.left);
+            t1.right = mergeTrees(t1.right, t2.right);
+            return t1;
+        }else if(t1 != null){
+            t1.left = mergeTrees(t1.left, null);
+            t1.right = mergeTrees(t1.right, null);
+            return t1;
+        }else{
+            t2.left = mergeTrees(null, t2.left);
+            t2.right = mergeTrees(null, t2.right);
+            return t2;
+        }
     }
 }
