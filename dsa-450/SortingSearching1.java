6a103a51a527cb98e20da9454cbf6f205a57b55d
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-03-25 19:23:57 +0530

    Solved: SortingSearching: First and Last occurance in Array

---
+++ dsa-450/SortingSearching1.java
@@ -0,0 +1,43 @@
+/*
+ * 
+ * link: https://practice.geeksforgeeks.org/problems/first-and-last-occurrences-of-x3116/1#
+ */
+
+ import java.util.ArrayList;
+
+public class SortingSearching1{
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.find(new long[]{1, 3, 5, 5, 5, 5, 67, 123, 125}, 9, 5));
+    }
+}
+
+class Solution{
+
+    ArrayList<Long> find(long arr[], int n, int x)
+    {
+        ArrayList<Long> indexes = new ArrayList<Long>();
+        
+        int i = 0;
+        while(i<n && arr[i] != x){
+            i++;
+        }
+        
+        if(i != n){
+            indexes.add(Long.valueOf(i));
+            
+            while(i<n && arr[i] == x){
+                i++;
+            }
+            indexes.add(Long.valueOf(i) - 1L);
+            return indexes;
+        }
+        
+        
+        indexes.add(-1L);
+        indexes.add(-1L);
+        return indexes;
+    }
+}
