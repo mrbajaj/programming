d68e8b83516577a06056faf3b59c71ce2fe29364
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-03-29 00:01:52 +0530

    Solved: LinkedList29: Sort List with values 0,1,2

--- dsa-450/LinkedList29.java
+++ dsa-450/LinkedList29.java
@@ -11,22 +11,23 @@ public class LinkedList29{
         LinkedList29 obj = new LinkedList29();
         Solution sol = new Solution();
 
-        ListNode a = new ListNode(1);
+        ListNode a = new ListNode(2);
         a.next = new ListNode(0);
         a.next.next = new ListNode(1);
         a.next.next.next = new ListNode(2);
         a.next.next.next.next = new ListNode(0);
-        a.next.next.next.next.next = new ListNode(0);
+        a.next.next.next.next.next = new ListNode(2);
         a.next.next.next.next.next.next = new ListNode(1);
         a.next.next.next.next.next.next.next = new ListNode(2);
+        a.next.next.next.next.next.next.next.next = new ListNode(0);
 
         System.out.print("Given:\t"); obj.printList(a);
         
-        ListNode ans = sol.segregate(a);
+        ListNode ans = sol.segregateByChangingLinks(a);
         System.out.print("Answer:\t"); obj.printList( ans );
         System.out.print("\n\n"); 
         
-
+        
     }
 
     public void printList(ListNode head){
@@ -37,24 +38,12 @@ public class LinkedList29{
         }
         System.out.println();
     }
-
-    public int lengthOfList(ListNode head){
-        int length = 0;
-        ListNode itr = head;
-        while(itr!=null){
-            length++;
-            itr = itr.next;
-        }
-        return length;
-        
-    }
-
 }
 
 class Solution
 {
     //Function to sort a linked list of 0s, 1s and 2s.
-    static ListNode segregate(ListNode head)
+    ListNode segregate(ListNode head)
     {
         ListNode curr = head;
         ListNode prev = head;
@@ -79,6 +68,48 @@ class Solution
 
         return head;
     }
+
+    ListNode segregateByChangingLinks(ListNode head)
+    {
+        ListNode curr = head, next;
+        ListNode zeroDummy = new ListNode(3), oneDummy =  new ListNode(3), twoDummy =  new ListNode(3);
+        ListNode zero = zeroDummy, one = oneDummy, two = twoDummy;
+
+        while(curr != null){
+            if(curr.val == 0){
+                zero.next = curr;
+                zero = zero.next;
+            } else if(curr.val == 1){
+                one.next = curr;
+                one = one.next;
+            } else if(curr.val == 2){
+                two.next = curr;
+                two = two.next;
+            }
+            next = curr.next;
+            curr.next = null;
+            curr = next;
+        }
+
+        this.printList(zeroDummy);
+        this.printList(oneDummy);
+        this.printList(twoDummy);
+
+        zero.next = (oneDummy.next!=null) ? (oneDummy.next) : (twoDummy.next);
+        one.next = twoDummy.next; 
+        
+        return zeroDummy.next;
+    }
+
+    void printList(ListNode head){
+        ListNode itr = head;
+        while(itr!=null){
+            System.out.print(itr +" ");
+            itr = itr.next;
+        }
+        System.out.println();
+    }
+
 }
 
 
