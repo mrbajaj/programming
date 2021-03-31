a8b2248cb5785ed7e68f0757856b96b8bc0a60e8
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-03-31 23:15:07 +0530

    Solved: Array, Easy

---
+++ leetcode/Problem1480.java
@@ -0,0 +1,28 @@
+/**
+ * URL: https://leetcode.com/problems/running-sum-of-1d-array/
+ * Tags: Easy, Array
+ * 
+ * Best Time Complexity:
+ * Best Space Complexity:
+ */
+import java.util.Arrays;
+
+public class Problem1480{
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(Arrays.toString(sol.runningSum(new int[]{1,2,3,4})));
+    }
+}
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        if(nums.length <= 1){
+            return nums;
+        }
+        for(int i = 1; i < nums.length; i++){
+            nums[i] = nums[i-1] + nums[i];
+        }
+        return nums;
+    }
+}
