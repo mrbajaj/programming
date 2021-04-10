2c58c1caf2c14aaaf98a5c12d3868e27d4790127
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-04-10 23:25:07 +0530

    Solved: Basic, Array

---
+++ leetcode/Problem344.java
@@ -0,0 +1,33 @@
+/**
+ * URL: https://leetcode.com/problems/sort-array-by-parity-ii/
+ * Tags: Array
+ * 
+ * Best Time Complexity:
+ * Best Space Complexity:
+ */
+import java.util.Arrays;
+
+ public class Problem344{
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        char[] arr = new char[]{'m','a','y','u','r'};
+        sol.reverseString(arr);
+        System.out.println(Arrays.toString(arr));
+    
+    }
+ }
+
+ class Solution {
+    public void reverseString(char[] s) {
+        int i = 0, j = s.length-1;
+        while(i < j){
+            char t = s[i];
+            s[i] = s[j];
+            s[j] = t;
+            i++;
+            j--;
+        }
+    }
+}
