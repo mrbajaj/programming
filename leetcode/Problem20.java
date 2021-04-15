61fb40a0888b47ad36139249bb2568ab59e4b1bc
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-04-15 23:38:07 +0530

    Solved: Easy, String, Stack

---
+++ leetcode/Problem20.java
@@ -0,0 +1,52 @@
+/**
+ * URL: https://leetcode.com/problems/valid-parentheses/
+ * Tags: Stack
+ * 
+ * Best Time Complexity:
+ * Best Space Complexity:
+ */
+import java.util.Stack;
+
+public class Problem20{
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println("Ans: " + sol.isValid("()[]{}"));
+
+        System.out.println("Ans: " + sol.isValid("(]"));
+    }
+
+}
+
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(int i = 0; i < s.length(); i++){
+            // System.out.println(stack.toString());
+            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{'){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                Character top = stack.pop();
+                if(top == '(' && s.charAt(i) != ')'){
+                    return false;
+                }
+                if(top == '{' && s.charAt(i) != '}'){
+                    return false;
+                }
+                if(top == '[' && s.charAt(i) != ']'){
+                    return false;
+                }
+            }
+        }
+
+        if(stack.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
