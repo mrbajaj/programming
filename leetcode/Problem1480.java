b0b7287c14eb220b4356695ebc5697dd2661044a
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-05-03 22:13:37 +0530

    Solved: Basic, Array

--- leetcode/Problem1480.java
+++ leetcode/Problem1480.java
@@ -17,9 +17,6 @@ public class Problem1480{
 
 class Solution {
     public int[] runningSum(int[] nums) {
-        if(nums.length <= 1){
-            return nums;
-        }
         for(int i = 1; i < nums.length; i++){
             nums[i] = nums[i-1] + nums[i];
         }
