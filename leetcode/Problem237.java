9ca6f964fd4285bbca276d57ef7edb5808ef5f34
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-05-14 00:54:15 +0530

    Solved: Easy, LinkedList

---
+++ leetcode/Problem237.java
@@ -0,0 +1,69 @@
+/**
+ * URL: 
+ * Tags: LinkedList
+ * 
+ * Best Time Complexity:
+ * Best Space Complexity:
+ */
+
+public class Problem237{
+    public static void main(String[] args) {
+        Problem237 obj = new Problem237();
+        Solution sol = new Solution();
+
+        ListNode a = new ListNode(1);
+        a.next = new ListNode(2);
+        a.next.next = new ListNode(3);
+        a.next.next.next = new ListNode(4);
+        a.next.next.next.next = new ListNode(5);
+        a.next.next.next.next.next = new ListNode(6);
+
+        System.out.print("Given:\t"); obj.printList(a);
+        
+        sol.deleteNode(a.next.next);
+
+        System.out.print("Answer:\t"); obj.printList(a);
+        
+    }
+
+    public void printList(ListNode head){
+        ListNode itr = head;
+        while(itr!=null){
+            System.out.print(itr +" ");
+            itr = itr.next;
+        }
+        System.out.println();
+    }
+
+    public int lengthOfList(ListNode head){
+        int length = 0;
+        ListNode itr = head;
+        while(itr!=null){
+            length++;
+            itr = itr.next;
+        }
+        return length;
+        
+    }
+
+}
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+class ListNode{
+    Integer val;
+    ListNode next;
+    ListNode(int v){
+        this.val = v;
+        this.next = null;
+    }
+    @Override
+    public String toString() {
+        return String.valueOf(this.val);
+    }
+}
