489801a5a1f0e5a68f2d81fac00db3c95fbf16b5
Mayur Bajaj <mayurkumarbajaj@gmail.com>
2021-07-01 23:58:53 +0530

    Archive2

--- leetcode/archive2/Problem617.java
+++ leetcode/archive2/Problem617.java
@@ -1,5 +1,5 @@
 /**
- * URL: https://leetcode.com/problems/merge-two-binary-trees/
+ * URL: 
  * Tags: Tree
  * 
  * Best Time Complexity:
@@ -21,30 +21,29 @@ class TreeNode {
     }
 }
 
-public class Problem617 {
+class Problem617 {
 
     public static void main(String[] args) {
         Problem617 obj = new Problem617();
-        Solution2 sol = new Solution2();
+        Solution sol = new Solution();
 
         TreeNode p = new TreeNode(1);
-        p.left = new TreeNode(3);  
-        p.right = new TreeNode(2);  
-        p.left.left = new TreeNode(5);  
-        
-        obj.printTree(p); System.out.println();
+        p.right = new TreeNode(2);
+        p.right.left = new TreeNode(3);
 
-        TreeNode q = new TreeNode(2);  
-        q.left = new TreeNode(1);  
-        q.right = new TreeNode(3);  
-        q.left.right = new TreeNode(4);  
+        obj.printTree(p); System.out.println();
+        
+        TreeNode q = new TreeNode(4);  
+        q.left = new TreeNode(2);  
+        q.right = new TreeNode(9);  
+        q.left.left = new TreeNode(3);  
+        q.left.right = new TreeNode(8);  
         q.right.right = new TreeNode(7);
 
-        obj.printTree(p); System.out.println();
-
+        obj.printTree(q); System.out.println();
         System.out.println("Ans: \n");
-        TreeNode r = sol.mergeTrees(p, q);
-        obj.printTree(r); System.out.println();
+        TreeNode ans = sol.mergeTrees(p,q);
+        obj.printTree(ans); System.out.println();
     }
 
     public void printTree(TreeNode root) {
@@ -61,48 +60,26 @@ public class Problem617 {
 }
 
 class Solution {
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if(t1 == null && t2 == null){
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null){
             return null;
         }
 
-        TreeNode newNode;
-        if(t1 != null && t2 != null){
-            newNode = new TreeNode(t1.val + t2.val);
-            newNode.left = mergeTrees(t1.left, t2.left);
-            newNode.right = mergeTrees(t1.right, t2.right);    
-        }else if(t1 != null){
-            newNode = t1;
-            newNode.left = mergeTrees(t1.left, null);
-            newNode.right = mergeTrees(t1.right, null);
+        TreeNode newNode = new TreeNode(0);
+        if(root1 != null && root2 != null){
+            newNode.val += root1.val;
+            newNode.val += root2.val;
+            newNode.left = mergeTrees(root1.left, root2.left);
+            newNode.right = mergeTrees(root1.right, root2.right);
+        }else if(root1 != null){
+            newNode.val += root1.val;
+            newNode.left = mergeTrees(root1.left, null);
+            newNode.right = mergeTrees(root1.right, null);
         }else{
-            newNode = t2;
-            newNode.left = mergeTrees(null, t2.left);
-            newNode.right = mergeTrees(null, t2.right);
+            newNode.val += root2.val;
+            newNode.left = mergeTrees(null, root2.left);
+            newNode.right = mergeTrees(null, root2.right);
         }
         return newNode;
-    }
-}
-
-class Solution2{
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if(t1 == null && t2 == null){
-            return null;
-        }
-
-        if(t1 != null && t2 != null){
-            t1.val = t1.val + t2.val;
-            t1.left = mergeTrees(t1.left, t2.left);
-            t1.right = mergeTrees(t1.right, t2.right);
-            return t1;
-        }else if(t1 != null){
-            t1.left = mergeTrees(t1.left, null);
-            t1.right = mergeTrees(t1.right, null);
-            return t1;
-        }else{
-            t2.left = mergeTrees(null, t2.left);
-            t2.right = mergeTrees(null, t2.right);
-            return t2;
-        }
     }
 }
